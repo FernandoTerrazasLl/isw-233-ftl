ba2127f4be468fd9d3c3f0358fa338b365fe7860
fernando <terrazasllanosfernando@gmail.com>
2026-02-20 15:17:46 -0400

    mejorando sintaxis e implementacion

--- walkie.py
+++ walkie.py
@@ -3,57 +3,55 @@ import sys
 import sounddevice as sd
 import numpy as np
 
-FORMATO_AUDIO = 'int8'    # Calidad básica (8 bits)
-CANALES = 1               # Mono: un solo canal de sonido
-FRECUENCIA = 44100        # Qué tan nítido se escucha
-MAX_FRAMES = 300000       # Cuánto tiempo grabamos
-FRAME_SIZE = 1            # Cada pedacito de sonido pesa 1 byte
+FORMATO_AUDIO = 'int8'    # calidad (8 bits)
+CANALES = 1               # mono: un solo canal de sonido
+FRECUENCIA = 44100        # que tan bien se escucha
+MAX_FRAMES = 65000        # bits que acepto
 
 def StartServer(port):
-    # Creamos el socket usando el protocolo de mensajes rápidos (UDP)
+    # Creamos socket          IPV4              UDP
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        # El programa lo conecto al puerto elegido
+        # conecto el programa al puerto 
         s.bind(('', port))
         print(f"Servidor listo. Estoy esperando que me mandes audio en el puerto {port}...")
         
         while True:
-            # Nos quedamos sentados esperando a que llegue un paquete de datos
-            data, addr = s.recvfrom(MAX_FRAMES * FRAME_SIZE)
+            # Esperamos el audio
+            data, addr = s.recvfrom(MAX_FRAMES)
 
-            frames_to_play = len(data) // FRAME_SIZE
+            frames_to_play = len(data)
 
-            # El sonido llega como binario, aquí lo convertimos en números
+            # convierto binario a números
             audio_data = np.frombuffer(data, dtype=np.int8)
 
-            print(f"¡Llegó audio! {frames_to_play}kb Reproduciendo lo que mandó: {addr}")
+            print(f"¡Llegó audio! {frames_to_play} bits Reproduciendo lo que mandó: {addr}")
 
-            # Hacemos que suene por los parlantes
             sd.play(audio_data, FRECUENCIA)
-            sd.wait() # Esperamos a que termine de sonar para no mezclar audios
+            sd.wait() # esperamos que suene para sonar lo demas
 
 def StartClient(port):
-    # Creamos el socket usando el protocolo de mensajes rápidos (UDP)
+    #Creamos socket          IPV4              UDP
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         server_ip = '127.0.0.1'
         print(f"Grabando...")
 
-        # Guarda el sonido como numeros
+        # guarda sonido como numero
         recording = sd.rec(MAX_FRAMES, samplerate=FRECUENCIA, channels=CANALES, dtype=FORMATO_AUDIO)
-        sd.wait() # Esperamos a que termines de hablar
+        sd.wait() 
 
-        # Convertimos esa lista de números a binario
+        # convertimos numeros a bytes
         data_to_send = recording.tobytes()
 
-        # Calculamos cuánto pesa todo el audio en bytes
+        # cuantos bytes tiene
         total_bytes = len(data_to_send)
 
-        # Mi compu no admite pedazos grandes, por eso lo mando de 8 en 8
+        # mandar de 8 en 8 para que aguante la compu
         CHUNK_SIZE = 8192
 
-        # Vamos mandando pedazo por pedazo hasta terminar
+        #for      inicio    fin         paso
         for i in range(0, total_bytes, CHUNK_SIZE):
-            chunk = data_to_send[i:i + CHUNK_SIZE]
-            # Lanzamos el pedazo a la IP y el puerto del servidor
+            chunk = data_to_send[i:i + CHUNK_SIZE] # corta el audio en bits de i a i+8bits
+            # enviamos el chunk al address
             s.sendto(chunk, (server_ip, port))
             
         print("Todo listo")
